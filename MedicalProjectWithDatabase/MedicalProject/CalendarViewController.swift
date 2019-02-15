fe87532af082197979ae606fd948fd41302e25bf
Ricky Yang <yangrhy@gmail.com>
2019-02-14 22:27:27 -0600

    Calendar Scene Added
    Calendar working, need to implement more.

--- MedicalProjectWithDatabase/MedicalProject/CalendarViewController.swift
+++ MedicalProjectWithDatabase/MedicalProject/CalendarViewController.swift
@@ -6,8 +6,10 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class CalendarViewController: UIViewController {
+    let formatter = DateFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,14 +18,34 @@ class CalendarViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    */
 
+}
+
+extension CalendarViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "2019 01 01")!
+        let endDate = formatter.date(from: "2019 12 31")!
+        
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        return parameters
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        
+        cell.dateLabel.text = cellState.text
+        return cell
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        let myCustomCell = cell as! CustomCell
+        myCustomCell.dateLabel.text = cellState.text
+    }
 }
---
+++ MedicalProjectWithDatabase/MedicalProject/CustomCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCell.swift
+//  MedicalProject
+//
+//  Created by Ricky Yang on 2/14/19.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+}
