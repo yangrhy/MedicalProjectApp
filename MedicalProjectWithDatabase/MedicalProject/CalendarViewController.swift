82ad27e8b2124e1ac79bb2c7c76f1962f7b7f8da
Ricky Yang <yangrhy@gmail.com>
2019-02-16 21:53:34 -0600

    Calendar Finished
    Calendar has been finished. Need to implement click date to list deliveries for day.

--- MedicalProjectWithDatabase/MedicalProject/CalendarViewController.swift
+++ MedicalProjectWithDatabase/MedicalProject/CalendarViewController.swift
@@ -10,23 +10,71 @@ import JTAppleCalendar
 
 class CalendarViewController: UIViewController {
     let formatter = DateFormatter()
-
     @IBOutlet weak var calendarView: JTAppleCalendarView!
+    @IBOutlet weak var month: UILabel!
+    @IBOutlet weak var year: UILabel!
+
     
-    func setupCalendarView() {
-        calendarView.minimumLineSpacing = 0
-        calendarView.minimumInteritemSpacing = 0
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupCalendarView()
         // Do any additional setup after loading the view.
     }
     
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func setupCalendarView() {
+        // calendar spacing
+        calendarView.minimumLineSpacing = 0
+        calendarView.minimumInteritemSpacing = 0
+        // Setup labels
+        calendarView.visibleDates { (visibleDates) in
+            self.setupCalendarLabels(from: visibleDates)
+        }
+        
+    }
+    
+    // Initialization of month and year labels upon first open
+    func setupCalendarLabels (from visibleDates: DateSegmentInfo) {
+        let date = visibleDates.monthDates.first!.date
+        
+        formatter.dateFormat = "yyyy"
+        year.text = formatter.string(from: date)
+        
+        formatter.dateFormat = "MMMM"
+        month.text = formatter.string(from: date)
+    }
+    
+    // controls display of what day is selected
+    func handleSelectedCell(view: JTAppleCell?, cellState: CellState) {
+        guard let cell = view as? CustomCell else { return }
+        if cellState.isSelected {
+            cell.viewSelected.isHidden = false
+        }
+        else {
+            cell.viewSelected.isHidden = true
+        }
+    }
+    
+    // gray out of days do not belong to the current month
+    func handleCellDateColor(view: JTAppleCell?, cellState: CellState) {
+        guard let cell = view as? CustomCell else { return }
+        
+        if cellState.isSelected {
+            cell.dateLabel.textColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        }
+        else {
+            if cellState.dateBelongsTo == .thisMonth {
+                cell.dateLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            }
+            else {
+                cell.dateLabel.textColor = #colorLiteral(red: 0.4912299313, green: 0.6373919675, blue: 0.8439119171, alpha: 1)
+            }
+        }
     }
 
 }
@@ -50,27 +98,31 @@ extension CalendarViewController: JTAppleCalendarViewDelegate {
         let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
         
         cell.dateLabel.text = cellState.text
-        if cellState.isSelected {
-            cell.viewSelected.isHidden = false
-        }
-        else {
-            cell.viewSelected.isHidden = true
-        }
+        handleSelectedCell(view: cell, cellState: cellState)
+        handleCellDateColor(view: cell, cellState: cellState)
         return cell
     }
     
     func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
         let myCustomCell = cell as! CustomCell
         myCustomCell.dateLabel.text = cellState.text
+        handleSelectedCell(view: cell, cellState: cellState)
+        handleCellDateColor(view: cell, cellState: cellState)
+
     }
     
     func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
-        guard let validCell = cell as? CustomCell else { return }
-        validCell.viewSelected.isHidden = false
+        handleSelectedCell(view: cell, cellState: cellState)
+        handleCellDateColor(view: cell, cellState: cellState)
+
     }
     
     func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
-    guard let validCell = cell as? CustomCell else { return }
-    validCell.viewSelected.isHidden = true
+        handleSelectedCell(view: cell, cellState: cellState)
+        handleCellDateColor(view: cell, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didScrollToDateSegmentWith visibleDates: DateSegmentInfo) {
+        setupCalendarLabels(from: visibleDates)
     }
 }
